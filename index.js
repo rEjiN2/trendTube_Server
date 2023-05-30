13af2c9a0eff28607bbc7b065d5c48346492ce34
rEjiN2 <rejin374@gmail.com>
2023-05-30 18:36:15 +0530

    corissuee

--- index.js
+++ index.js
@@ -30,11 +30,11 @@ const connect = () => {
       throw err;
     });
 };
-app.options('*', cors());
+
 
 app.use(
   cors({
-    origin: "https://www.trendtube.online",
+    origin: ["https://www.trendtube.online","https://api.trendtube.online"],
     credentials: true,
     methods: ['GET', 'POST' ,'PUT' ,'DELETE']
   })
@@ -43,14 +43,14 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://www.trendtube.online");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Content-Type", "application/json;charset=UTF-8");
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "https://www.trendtube.online");
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//   res.header("Content-Type", "application/json;charset=UTF-8");
+//   res.header("Access-Control-Allow-Credentials", true);
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 const addAccessTokenToHeader = async (req, res, next) => {
   const accessToken = req.cookies["access_token"];
